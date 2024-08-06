ef98e7e29df38c8b32f3db075c7da55b56ee3e5e
fitronansori <fitronansori7@gmail.com>
2024-08-06 20:41:41 +0800

    chore: Add Hero component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/layouts/Header";
+import Hero from "./components/layouts/Hero";
 
 export default function App() {
   return (
     <>
       <Header />
+      <Hero />
     </>
   );
 }
