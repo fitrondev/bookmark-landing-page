e883a931254718b211e02bfb4c21e5ae3b6449f9
fitronansori <fitronansori7@gmail.com>
2024-08-07 10:10:03 +0800

    chore: Update Title component font size to 3xl

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -6,7 +6,7 @@ type TitleProps = {
 const Title = ({ title, desc }: TitleProps) => {
   return (
     <div className="lg:max-w-[490px] lg:mx-auto text-center space-y-5">
-      <h3 className="text-2xl font-semibold">{title}</h3>
+      <h3 className="text-3xl font-semibold">{title}</h3>
       <p className="text-muted-foreground">{desc}</p>
     </div>
   );
