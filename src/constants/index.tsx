f5e4acc49682241a4c5ae294af3f51f88ff3e6c1
fitronansori <fitronansori7@gmail.com>
2024-08-07 10:09:17 +0800

    feat: Add browser constants for Chrome, Firefox, and Opera

---
+++ src/constants/index.tsx
@@ -0,0 +1,27 @@
+import LogoChrome from "@/assets/logo-chrome.svg";
+import LogoFirefox from "@/assets/logo-firefox.svg";
+import LogoOpera from "@/assets/logo-opera.svg";
+
+export const BROWSERS = [
+  {
+    id: 1,
+    name: "Chrome",
+    logo: LogoChrome,
+    version: "62",
+    url: "https://chrome.google.com/webstore/detail/lofi/ibjgjdf",
+  },
+  {
+    id: 2,
+    name: "Firefox",
+    logo: LogoFirefox,
+    version: "55",
+    url: "https://addons.mozilla.org/en-US/firefox/addon/lofi/",
+  },
+  {
+    id: 3,
+    name: "Opera",
+    logo: LogoOpera,
+    version: "46",
+    url: "https://addons.opera.com/en/extensions/details/lofi/",
+  },
+];
