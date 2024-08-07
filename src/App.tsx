2302a2b7097be85b384fc64b3557300770cfe173
fitronansori <fitronansori7@gmail.com>
2024-08-07 10:56:12 +0800

    feat: Add Footer component to App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Downloads from "./components/layouts/Downloads";
 import Faq from "./components/layouts/Faq";
 import Features from "./components/layouts/Features";
+import Footer from "./components/layouts/Footer";
 import Header from "./components/layouts/Header";
 import Hero from "./components/layouts/Hero";
 import Join from "./components/layouts/Join";
@@ -14,6 +15,7 @@ export default function App() {
       <Downloads />
       <Faq />
       <Join />
+      <Footer />
     </>
   );
 }
